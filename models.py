b9f6eb1fbc781982289b85d5e0f5e40ad85efcb0
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-09-04 11:47:11 -0300

    add email in pacientes

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 import sqlalchemy as sa
 from typing import List
 from db_definition import db
+from sqlalchemy import String
 
 class Paciente(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(String(50))
     profissionais: Mapped[List["Profissional"]] = relationship(
         secondary="atendimento", back_populates="pacientes"
     )
